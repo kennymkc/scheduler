a7ad8af6eec06594bb52a4df87559b0dfc96d524
Kennie <Kenneth Chan>
2023-01-10 16:01:52 -0500

    add DayListItem with stories

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,7 +5,7 @@ import "components/Button.scss";
 
 export default function Button(props) {
 
-   let buttonClass = classNames("button", {
+   const buttonClass = classNames("button", {
       "button--confirm": props.confirm,
       "button--danger": props.danger
    });
