f43ff864ec43e2e0537837316867b38c0b3cdb32
Kennie <Kenneth Chan>
2023-01-12 17:03:21 -0500

    import axios & scheduler api remove hardcoded data for days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
+import axios from "axios";
 
 import "components/Application.scss";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   "1": {
@@ -63,7 +46,15 @@ const appointments = {
 
 export default function Application(props) {
 
-  const [day, setDay] = useState("Monday")
+  const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/days").then((response) => {
+      console.log(response)
+      setDays(response.data)
+    })
+  }, [])
   
   return (
     <main className="layout">
