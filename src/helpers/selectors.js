b1400e14e2def2f4d474dd3265185b321dabfeb7
Kennie <Kenneth Chan>
2023-01-13 16:21:01 -0500

    create getInterview helper with test and fetch interviewer data

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,5 +1,8 @@
 export function getAppointmentsForDay(state, day) {
   const filteredDays = state.days.find(selectedDay => selectedDay.name === day);
-  return !filteredDays ? [] : filteredDays.appointments.map(id => state.appointments[id])
-  
+  return !filteredDays ? [] : filteredDays.appointments.map(id => state.appointments[id]);
+}
+
+export function getInterview(state, interview) {
+  return !interview ? null : { ...interview, interviewer: state.interviewers[interview.interviewer] };
 }
